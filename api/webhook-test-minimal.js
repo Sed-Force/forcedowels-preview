73a964905aa60c04b0db3601105a7554d2fa798b
Sed-Force <scott@forcecabinets.com>
2025-11-06 09:26:24 -0700

    Test with email builder imports

--- api/webhook-test-minimal.js
+++ api/webhook-test-minimal.js
@@ -3,6 +3,8 @@ export const config = { runtime: 'nodejs' };
 
 import Stripe from 'stripe';
 import { sql, nextCounter } from './_lib/db.js';
+import { buildInternationalOrderConfirmationEmail } from './_lib/email/internationalOrderConfirmation.js';
+import { buildInternationalInternalNotificationHTML } from './_lib/email/internationalInternalNotification.js';
 
 const stripeSecret = process.env.STRIPE_SECRET_KEY;
 const stripe = stripeSecret ? new Stripe(stripeSecret) : null;
@@ -10,9 +12,10 @@ const stripe = stripeSecret ? new Stripe(stripeSecret) : null;
 export default async function handler(req, res) {
   return res.status(200).json({
     success: true,
-    message: 'Minimal webhook works with db import',
+    message: 'Minimal webhook works with ALL imports',
     has_stripe: !!stripe,
     has_sql: !!sql,
-    has_nextCounter: !!nextCounter
+    has_nextCounter: !!nextCounter,
+    has_email_builders: !!(buildInternationalOrderConfirmationEmail && buildInternationalInternalNotificationHTML)
   });
 }
