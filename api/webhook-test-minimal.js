f01caaaad7f1be09273a95e1ee64c463c28d7a1d
Sed-Force <scott@forcecabinets.com>
2025-11-06 09:25:04 -0700

    Test with db.js import

--- api/webhook-test-minimal.js
+++ api/webhook-test-minimal.js
@@ -2,6 +2,7 @@
 export const config = { runtime: 'nodejs' };
 
 import Stripe from 'stripe';
+import { sql, nextCounter } from './_lib/db.js';
 
 const stripeSecret = process.env.STRIPE_SECRET_KEY;
 const stripe = stripeSecret ? new Stripe(stripeSecret) : null;
@@ -9,7 +10,9 @@ const stripe = stripeSecret ? new Stripe(stripeSecret) : null;
 export default async function handler(req, res) {
   return res.status(200).json({
     success: true,
-    message: 'Minimal webhook works',
-    has_stripe: !!stripe
+    message: 'Minimal webhook works with db import',
+    has_stripe: !!stripe,
+    has_sql: !!sql,
+    has_nextCounter: !!nextCounter
   });
 }
